6cbf1ab17d14163c5856cc75e8637b7ac261e95c
ElshefaaPharmacy <elshefaa@example.com>
2025-11-20 18:11:02 +0200

    edit src

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Alfurqan-website/',
+  // base: '/Alfurqan-website/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
